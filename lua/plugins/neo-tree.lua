b4fb4d0526b3e5644feed16ecec5cf4509f0f06c
Abdul Rafay Modi <abdul.rafay+bykea.git@bykea.com>
2024-04-01 13:56:59 +0500

    Updated neo-tree.lua to hide specific folders

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,6 +6,16 @@ return {
       "nvim-tree/nvim-web-devicons", 
       "MunifTanjim/nui.nvim",
     },
+    opts = {
+      filtered_items = {
+        hide_by_name = {
+          ".git",
+          "node_modules",
+          "package-lock.json"
+        },
+        never_show = { ".git" }
+      } 
+    }
   config = function () 
     vim.keymap.set('n', '<C-b>', ':Neotree filesystem toggle left')    
   end
