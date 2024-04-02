f59dcc06093286bf3d7ea22f16247f2d00f8a343
Abdul Rafay Modi <74718326+AlphaRomeoMike@users.noreply.github.com>
2024-04-02 09:47:03 +0500

    - Configure LSP  (#1)
    * Configuring lsps
    
    * Fixing LSP
    
    * Setup tsserver
    
    * fix: formatting

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,22 +1,22 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", 
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      filtered_items = {
-        hide_by_name = {
-          ".git",
-          "node_modules",
-          "package-lock.json"
-        },
-        never_show = { ".git" }
-      } 
-    },
-  config = function () 
-    vim.keymap.set('n', '<C-b>', ':Neotree filesystem toggle left')    
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	opts = {
+		filtered_items = {
+			hide_by_name = {
+				".git",
+				"node_modules",
+				"package-lock.json",
+			},
+			never_show = { ".git" },
+		},
+	},
+	config = function()
+		vim.keymap.set("n", "<C-b>", ":Neotree filesystem toggle left")
+	end,
 }
