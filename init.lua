d4272f0951fe60f08c7b589f206750438122202c
Abdul Rafay Modi <abdul.rafay+bykea.git@bykea.com>
2024-04-02 09:44:09 +0500

    fix: formatting

--- init.lua
+++ init.lua
@@ -9,14 +9,14 @@ vim.g.mapLeader = " "
 -- Install Lazy.Nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -24,4 +24,3 @@ local opts = {}
 
 -- Require plugins
 require("lazy").setup("plugins", opts)
-
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,25 @@
-return { 
-  'nvim-treesitter/nvim-treesitter', 
-  build = ":TSUpdate", 
-  config = function ()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "lua", "javascript", "typescript", "html", "angular", "json", "yaml", "bash", "css", "csv", "diff", "java" },
-      highlight = { enable = true },
-      indent = { enable = true } 
-    })
-  end
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = {
+				"lua",
+				"javascript",
+				"typescript",
+				"html",
+				"angular",
+				"json",
+				"yaml",
+				"bash",
+				"css",
+				"csv",
+				"diff",
+				"java",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
